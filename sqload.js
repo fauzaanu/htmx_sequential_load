5438edcede4abede41a8c875c1c3ea1d49867002
Fauzaan Gasim <86226565+fauzaanu@users.noreply.github.com>
2024-08-03 13:58:28 +0500

    Create sqload.js

---
+++ sqload.js
@@ -0,0 +1,27 @@
+(function() {
+  const HTMXSequentialLoad = {
+    init: function(config = {}) {
+      this.delay = config.delay || 1000;
+      this.initialDelay = config.initialDelay || 100;
+      this.triggerEvent = 'sqload';
+      
+      document.addEventListener("DOMContentLoaded", () => this.setupSequentialLoad());
+    },
+
+    setupSequentialLoad: function() {
+      const items = document.querySelectorAll(`[hx-trigger="${this.triggerEvent}"]`);
+      
+      const triggerNextLoad = (index) => {
+        if (index < items.length) {
+          const item = items[index];
+          item.dispatchEvent(new Event(this.triggerEvent));
+          setTimeout(() => triggerNextLoad(index + 1), this.delay);
+        }
+      };
+
+      setTimeout(() => triggerNextLoad(0), this.initialDelay);
+    }
+  };
+
+  window.HTMXSequentialLoad = HTMXSequentialLoad;
+})();
